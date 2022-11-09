9470a1611c4ed4dce570778bb0bd34f07d2adb43
Banzaika <herovichrick@gmail.com>
2022-11-09 12:22:52 +0300

    todo without frontend

--- todoapp/todo/models.py
+++ todoapp/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = CharField(max_lenght=150)
+    description = CharField(max_lenght=300, blank=True)
+    date = DateTimeField(auto_now_add=True)
+    done = BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title
